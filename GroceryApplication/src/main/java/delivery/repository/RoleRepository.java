bf294181b4ae6485accfeee951a4cd9f57bb6ab0
akyadav4710 <163339150+akyadav4710@users.noreply.github.com>
2024-03-14 18:50:12 +0530

    Delete GroceryApplication directory

--- GroceryApplication/src/main/java/delivery/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package delivery.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import delivery.model.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-
-}
